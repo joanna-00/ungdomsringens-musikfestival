a019af3d413e49742dad3878789b7f4dea4d73f9
Joanna Jankowska <1081578@ucn.dk>
2020-03-30 13:18:33 +0200

    Tabs toggle animations

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,6 +2,9 @@ let burger = document.getElementById('burger');
 let exit = document.getElementById('exit-icon');
 let menu = document.getElementById('menu-icon');
 let nav = document.getElementById('navigation');
+let languageBtn = document.getElementsByClassName('lang')[0];
+let popup = document.getElementsByClassName('lang-popup')[0];
+const articleHeaders = document.querySelectorAll('.article-header');
 
 burger.addEventListener('click', function(){
 	nav.classList.toggle('hidden');
@@ -9,9 +12,14 @@ burger.addEventListener('click', function(){
 	menu.classList.toggle('hidden');
 });
 
-let languageBtn = document.getElementsByClassName('lang')[0];
-let popup = document.getElementsByClassName('lang-popup')[0];
 
 languageBtn.addEventListener('click', function(){
 	popup.classList.toggle('hidden');
 });
+
+articleHeaders.forEach(elem => {
+	elem.addEventListener('click', () => {
+		let siblingP = elem.parentElement.getElementsByTagName('p')[0]
+		siblingP.classList.toggle('hidden-display')
+	})
+})
