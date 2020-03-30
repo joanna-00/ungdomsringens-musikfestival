0f9ed35ed85e8dddf7741646da17a257ef6213bb
Joanna Jankowska <1081578@ucn.dk>
2020-03-30 14:29:34 +0200

    Table arrow side-scroll

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,6 +6,9 @@ let languageBtn = document.getElementsByClassName('lang')[0];
 let popup = document.getElementsByClassName('lang-popup')[0];
 const arrows = document.querySelectorAll('.fa-chevron-down');
 const articleHeaders = document.querySelectorAll('.article-header');
+// const scrollable = document.querySelectorAll('.scrollable')
+const scrollArrow = document.querySelectorAll('.scroll-arrow')
+
 
 burger.addEventListener('click', function(){
 	nav.classList.toggle('hidden');
@@ -31,3 +34,22 @@ articleHeaders.forEach(elem => {
 		siblingP.classList.toggle('hidden-display')
 	})
 })
+
+
+scrollArrow.forEach(elem => {
+	elem.addEventListener('click', () => {
+		let scrollable = elem.parentElement.parentElement.querySelector('.scrollable')
+		console.log(scrollable)
+		scrollable.scrollBy({
+			// top: 100,
+			left: 200,
+			behavior: 'smooth'
+		  });
+	})
+});
+
+
+
+// scrollable.forEach(elem => {
+// 	elem.scrollLeft
+// })
