002912a19fccbd4ce7fe4638879b82094fb33ed6
Joanna Jankowska <1081578@ucn.dk>
2020-03-30 13:30:33 +0200

    Fix arrow click event, change p font weight

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,6 +4,7 @@ let menu = document.getElementById('menu-icon');
 let nav = document.getElementById('navigation');
 let languageBtn = document.getElementsByClassName('lang')[0];
 let popup = document.getElementsByClassName('lang-popup')[0];
+const arrows = document.querySelectorAll('.fa-chevron-down');
 const articleHeaders = document.querySelectorAll('.article-header');
 
 burger.addEventListener('click', function(){
@@ -16,6 +17,13 @@ burger.addEventListener('click', function(){
 languageBtn.addEventListener('click', function(){
 	popup.classList.toggle('hidden');
 });
+
+arrows.forEach(elem => {
+	elem.addEventListener('click', () => {
+		let siblingP = elem.parentElement.getElementsByTagName('p')[0]
+		siblingP.classList.toggle('hidden-display')
+	})
+})
 
 articleHeaders.forEach(elem => {
 	elem.addEventListener('click', () => {
